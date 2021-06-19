e6ea6601cf9e39ef58de6da07a9cafea23df3368
juchos <juan121187@gmail.com>
2021-06-19 19:14:36 +0200

    Se sube todo el proyecto

--- Ferreteria/Ferreteria/settings.py
+++ Ferreteria/Ferreteria/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cliente',
     'productos',
+    'factura',
+    'GuiaRemision',
+    'Ventas',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
